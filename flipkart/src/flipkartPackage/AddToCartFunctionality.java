3217d7fdfb8a9a230fec617dcacc2f6e7a112fe8
Amit7602 <amitdhumal1397@gmail.com>
2022-04-21 06:57:27 +0530

    commit1
    add to cart update

---
+++ flipkart/src/flipkartPackage/AddToCartFunctionality.java
@@ -0,0 +1,11 @@
+package flipkartPackage;
+
+public class AddToCartFunctionality {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Add to cart changes is done as per the the customer requirement");
+
+	}
+
+}
